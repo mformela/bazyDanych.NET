a259f956cf08e0aebff50b44319da878ed2ab92a
Marta Formela <formela.marta@gmail.com>
2017-11-18 11:57:39 +0100

    start

---
+++ BazyDanych/BazyDanych/Controllers/TestDBmodelsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BazyDanych.Models;
+
+namespace BazyDanych.Controllers
+{
+    public class TestDBmodelsController : Controller
+    {
+        private AppContext db = new AppContext();
+
+        // GET: TestDBmodels
+        public ActionResult Index()
+        {
+            return View(db.TestDbmodels.ToList());
+        }
+
+        // GET: TestDBmodels/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TestDBmodel testDBmodel = db.TestDbmodels.Find(id);
+            if (testDBmodel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(testDBmodel);
+        }
+
+        // GET: TestDBmodels/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TestDBmodels/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Prop")] TestDBmodel testDBmodel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TestDbmodels.Add(testDBmodel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(testDBmodel);
+        }
+
+        // GET: TestDBmodels/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TestDBmodel testDBmodel = db.TestDbmodels.Find(id);
+            if (testDBmodel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(testDBmodel);
+        }
+
+        // POST: TestDBmodels/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Prop")] TestDBmodel testDBmodel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(testDBmodel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(testDBmodel);
+        }
+
+        // GET: TestDBmodels/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TestDBmodel testDBmodel = db.TestDbmodels.Find(id);
+            if (testDBmodel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(testDBmodel);
+        }
+
+        // POST: TestDBmodels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TestDBmodel testDBmodel = db.TestDbmodels.Find(id);
+            db.TestDbmodels.Remove(testDBmodel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BazyDanych/BazyDanych/Models/AppContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BazyDanych.Models
+{
+    public class AppContext : DbContext
+    {
+        public AppContext() : base ("TestDB") /*wprowadzamy connection string do bazy danych - będziemy się z nią łączyć */
+        {
+        
+        }
+        
+        public DbSet<TestDBmodel> TestDbmodels { get; set; }
+        
+       
+    }
+}
